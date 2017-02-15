db0735debb27807e85f349cfad6bc2374581f51b
Nikola Yurukov <nyurukov@shutterfly.com>
2017-02-16 00:33:56 +0200

    [Dev] Implemented Fibonacci Heap

--- fibheap/fibheap.go
+++ fibheap/fibheap.go
@@ -69,6 +69,11 @@ http://www.keithschwarz.com/interesting/code/?dir=fibonacci-heap
 
 package fibheap
 
+import (
+	"fmt"
+	"math"
+)
+
 /******************************************
  ************** INTERFACE *****************
  ******************************************/
@@ -77,27 +82,33 @@ package fibheap
 // with only floating-point priorities and no user data attached.
 type FloatingFibonacciHeap interface {
 	// Adds and element to the heap
-	Enqueue(priority float64) float64
+	Enqueue(priority float64) *Entry
 	// Returns the minimum element in the heap
-	Min() float64
+	Min() (*Entry, error)
 	// Is the heap empty?
 	IsEmpty() bool
 	// The number of elements in the heap
 	Size() uint
 	// Removes and returns the minimal element
 	// in the heap
-	DequeueMin() float64
+	DequeueMin() (*Entry, error)
 	// Decreases the key of the given element
 	// and sets it to the new given priority
-	// returns the new priority if succesfully set
-	DecreaseKey(oldPriority, newPriority float64) float64
+	// returns the node if succesfully set
+	DecreaseKey(node *Entry, newPriority float64) (*Entry, error)
 	// Deletes the given element in the heap
-	// Will use the numeric value of the priority
-	// as an identifier.
-	// TODO: How do we deal with doubles?
-	Delete(priority float64) float64
+	Delete(node *Entry) error
 	// Will merge two heaps
-	Merge(otherHeap *FloatingFibonacciHeap) FloatingFibonacciHeap
+	Merge(otherHeap FloatingFibonacciHeap) (FloatingFibonacciHeap, error)
+}
+
+// Entry is the entry type that will be used
+// for each node of the Fibonacci heap
+type Entry struct {
+	degree                    int
+	marked                    bool
+	next, prev, child, parent *Entry
+	priority                  float64
 }
 
 /******************************************
@@ -105,35 +116,52 @@ type FloatingFibonacciHeap interface {
  ******************************************/
 
 type fibHeap struct {
-	min  *entry // The minimal element
-	size uint
+	min  *Entry // The minimal element
+	size uint   // Size of the heap
 }
 
-type entry struct {
-	degree                    int
-	isMarked                  bool
-	next, prev, child, parent *entry
-	priority                  float64
+// ****************
+// HELPER FUNCTIONS
+// ****************
+
+func newEntry(priority float64) *Entry {
+	result := new(Entry)
+	result.degree = 0
+	result.marked = false
+	result.child = nil
+	result.parent = nil
+	result.next = result
+	result.prev = result
+	result.priority = priority
+	return result
 }
 
+// ***********
 // ACTUAL CODE
+// ***********
 
+/*
+NewFloatFibHeap creates a new, empty, Fibonacci heap object.
+
+Remember that it's actually *fibHeap and not fibHeap
+that fulfills the contract of the interface.
+*/
 func NewFloatFibHeap() FloatingFibonacciHeap { return &fibHeap{nil, 0} }
 
-func (heap *fibHeap) Enqueue(priority float64) float64 {
-	//result := newEntry(priority)
+func (heap *fibHeap) Enqueue(priority float64) *Entry {
+	singleton := newEntry(priority)
 
 	// Merge singleton list with heap
-
+	heap.min = mergeLists(heap.min, singleton)
 	heap.size++
-	return priority
+	return singleton
 }
 
-func (heap *fibHeap) Min() float64 {
+func (heap *fibHeap) Min() (*Entry, error) {
 	if heap.IsEmpty() {
-		return -42
+		return nil, fmt.Errorf("Trying to get minimum element of empty heap")
 	}
-	return heap.min.priority
+	return heap.min, nil
 }
 
 func (heap *fibHeap) IsEmpty() bool {
@@ -144,16 +172,124 @@ func (heap *fibHeap) Size() uint {
 	return heap.size
 }
 
-func (heap *fibHeap) DequeueMin() float64 {
-	return 42.0
+func (heap *fibHeap) DequeueMin() (*Entry, error) {
+	if heap.IsEmpty() {
+		return nil, fmt.Errorf("Heap is empty")
+	}
+
+	heap.size--
+
+	// Copy pointer. Will need it later.
+	min := heap.min
+
+	if min.next == min { // This is the only root node
+		heap.min = nil
+	} else { // There are more root nodes
+		heap.min.prev.next = heap.min.next
+		heap.min.next.prev = heap.min.prev
+		heap.min = heap.min.next // Arbitrary element of the root list
+	}
+
+	if min.child != nil {
+		// Keep track of the first visited node
+		curr := min.child
+		for ok := true; ok; ok = (curr != min.child) {
+			curr.parent = nil
+			curr = curr.next
+		}
+	}
+
+	heap.min = mergeLists(heap.min, min.child)
+
+	if heap.min == nil {
+		// If there are no entries left, we're done.
+		return min, nil
+	}
+
+	treeSlice := make([]*Entry, 0, heap.size)
+	toVisit := make([]*Entry, 0, heap.size)
+
+	for curr := heap.min; len(toVisit) == 0 || toVisit[0] != curr; curr = curr.next {
+		toVisit = append(toVisit, curr)
+	}
+
+	for _, curr := range toVisit {
+		for {
+			for curr.degree >= len(treeSlice) {
+				treeSlice = append(treeSlice, nil)
+			}
+
+			if treeSlice[curr.degree] == nil {
+				treeSlice[curr.degree] = curr
+				break
+			}
+
+			other := treeSlice[curr.degree]
+			treeSlice[curr.degree] = nil
+
+			// Determine which of two trees has the smaller root
+			var minT, maxT *Entry
+			if other.priority < curr.priority {
+				minT = other
+				maxT = curr
+			} else {
+				minT = curr
+				maxT = other
+			}
+
+			// Break max out of the root list,
+			// then merge it into min's child list
+			maxT.next.prev = maxT.prev
+			maxT.prev.next = maxT.next
+
+			// Make it a singleton so that we can merge it
+			maxT.prev = maxT
+			maxT.next = maxT
+			minT.child = mergeLists(minT.child, maxT)
+
+			// Reparent max appropriately
+			maxT.parent = minT
+
+			// Clear max's mark, since it can now lose another child
+			maxT.marked = false
+
+			// Increase min's degree. It has another child.
+			minT.degree++
+
+			// Continue merging this tree
+			curr = minT
+		}
+
+		/* Update the global min based on this node.  Note that we compare
+		 * for <= instead of < here.  That's because if we just did a
+		 * reparent operation that merged two different trees of equal
+		 * priority, we need to make sure that the min pointer points to
+		 * the root-level one.
+		 */
+		if curr.priority <= heap.min.priority {
+			heap.min = curr
+		}
+	}
+
+	// All done. Return minimum element and no error
+	return min, nil
 }
 
-func (heap *fibHeap) DecreaseKey(oldPriority, newPriority float64) float64 {
-	return newPriority
+func (heap *fibHeap) DecreaseKey(node *Entry, newPriority float64) (*Entry, error) {
+
+	if newPriority > node.priority {
+		return nil, fmt.Errorf("The given new priority is larger than the old")
+	}
+
+	decreaseKeyUnchecked(heap, node, newPriority)
+	return node, nil
 }
 
-func (heap *fibHeap) Delete(priority float64) float64 {
-	return priority
+func (heap *fibHeap) Delete(node *Entry) error {
+
+	decreaseKeyUnchecked(heap, node, -math.MaxFloat64)
+	heap.DequeueMin()
+	return nil
 }
 
 /*
@@ -163,32 +299,94 @@ func (heap *fibHeap) Delete(priority float64) float64 {
  * continue to use those heaps, but be aware that they will be empty
  * after this call completes.
  */
-func (heap *fibHeap) Merge(other *FloatingFibonacciHeap) FloatingFibonacciHeap {
-	//result := new(FibHeap)
-	return NewFloatFibHeap()
+func (heap *fibHeap) Merge(other FloatingFibonacciHeap) (FloatingFibonacciHeap, error) {
 
-	//result.size = fibHeap.size + other.size
-	//result.min = fibHeap.mergeLists(other)
-	//return result
-}
-
-/*func (fibHeap *FibHeap) delMin() *entry {
-	if fibHeap.IsEmpty() {
-		return nil
+	otherHeap, ok := other.(*fibHeap)
+	if !ok {
+		// throw an error
+		return nil, fmt.Errorf("The passed object is of type %T, not of internal type *fibHeap. Please provide your own implementation of merge", other)
 	}
 
-	// TODO: fix
-	defer func() { fibHeap.size-- }()
-	minElem := fibHeap.min
-	return nil
+	resultSize := heap.size + otherHeap.size
+
+	resultMin := mergeLists(heap.min, otherHeap.min)
+
+	heap.min = nil
+	otherHeap.min = nil
+	heap.size = 0
+	otherHeap.size = 0
+
+	return &fibHeap{resultMin, resultSize}, nil
 }
 
-// HELPER FUNCTIONS
+func mergeLists(one, two *Entry) *Entry {
+	if one == nil && two == nil {
+		return nil
+	} else if one != nil && two == nil {
+		return one
+	} else if one == nil && two != nil {
+		return two
+	}
+	// Both trees non-null; actually do the merge.
+	oneNext := one.next
+	one.next = two.next
+	one.next.prev = one
+	two.next = oneNext
+	two.next.prev = two
 
-func newEntry(priority float64) entry {
-	result := new(entry)
-	result.next = &result
-	result.prev = &result
-	result.priority = priority
-	return result
-}*/
+	if one.priority < two.priority {
+		return one
+	}
+	return two
+
+}
+
+func decreaseKeyUnchecked(heap *fibHeap, node *Entry, priority float64) {
+	node.priority = priority
+
+	if node.parent != nil && node.priority <= node.parent.priority {
+		cutNode(heap, node)
+	}
+
+	if node.priority <= heap.min.priority {
+		heap.min = node
+	}
+}
+
+func cutNode(heap *fibHeap, node *Entry) {
+	node.marked = false
+
+	if node.parent == nil {
+		return
+	}
+
+	// Rewire siblings if it has any
+	if node.next != node {
+		node.next.prev = node.prev
+		node.prev.next = node.next
+	}
+
+	// Rewrite pointer if this is the representative child node
+	if node.parent.child == node {
+		if node.next != node {
+			node.parent.child = node.next
+		} else {
+			node.parent.child = nil
+		}
+	}
+
+	node.parent.degree--
+
+	node.prev = node
+	node.next = node
+	heap.min = mergeLists(heap.min, node)
+
+	// cut parent recursively if marked
+	if node.parent.marked {
+		cutNode(heap, node.parent)
+	} else {
+		node.parent.marked = true
+	}
+
+	node.parent = nil
+}
